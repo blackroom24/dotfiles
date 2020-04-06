0526a78a2784f9a3ffc7422d5025c561f5ee2779
Sanket Debnath <sanketdebnath24@gmail.com>
2020-04-07 00:07:48 +0530

    Added configs for doom emacs

---
+++ .config/doom/config.el
@@ -0,0 +1,14 @@
+;; setting font
+(setq doom-font (font-spec :family "Source Code Pro Medium" :size 21))
+
+;; setting theme
+(setq doom-theme 'doom-city-lights)
+
+;; setting org directory
+(setq org-directory "~/org/")
+
+;; setting lines
+(setq display-line-numbers-type 'relative)
+
+;; setting neotree
+(setq neo-window-fixed-size nil)
